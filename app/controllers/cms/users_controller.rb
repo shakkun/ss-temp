db4f60c3daf5f4c152c489a54450b4cc9c5f0f6d
Yukiko Ishihara <shakkun321@gmail.com>
2015-03-03 09:59:22 +0900

    Update users_controller.rb

--- app/controllers/cms/users_controller.rb
+++ app/controllers/cms/users_controller.rb
@@ -6,6 +6,7 @@ class Cms::UsersController < ApplicationController
   model Cms::User
 
   navi_view "cms/main/navi"
+  menu_view "cms/users/menu"
 
   private
     def set_crumbs
@@ -31,4 +32,21 @@ class Cms::UsersController < ApplicationController
       raise "403" unless @item.allowed?(:edit, @cur_user, site: @cur_site)
       render_update @item.update
     end
+    
+  def download
+    require "csv"
+
+    header = ["name", "email", "type", "groups", "cms_roles"]
+
+    group_ids = { "1" => "シラサギ市", "2" => "シラサギ市/企画政策部", "3" => "シラサギ市/企画政策部/広報課", "4" => "シラサギ市/企画政策部/政策課", "5" => "シラサギ市/危機管理部", "6" => "シラサギ市/危機管理部/管理課", "7" => "シラサギ市/危機管理部/防災課" }
+    cms_role_ids = { "1" => "サイト管理者", "2" => "記事編集権限" }
+
+    CSV.generate(list = "", :headers => header, :write_headers => true) do |csv|
+      @model.each do |u|
+        csv << [u.name, u.email, group_ids["#{u.group_ids.join("\n")}"], cms_role_ids["#{u.cms_role_ids.join("\n")}"]]
+      end
+    end
+
+    send_data(list, :type => 'text/csv', :filename => 'users.csv')
+  end
 end
